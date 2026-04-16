e155dc0f9884b6e4ae5d225b66a2ad957b3a00ca
Eduardo Grando Lopes <eduardogl281207@gmail.com>
2026-04-15 22:36:10 -0300

    tentetiva de modularizacao e tal

---
+++ funcoes.c
@@ -0,0 +1,96 @@
+#include "funcoes.h"
+
+void showTab(char tab[3][3]){
+  int i, j;
+  static int count = 0;
+
+
+  printf("Rodada [%d]\n", (++count));
+
+  for(i = 0; i < 3; i++){
+
+    if(i > 0) {
+      printf("|");
+    }
+    printf("\n ___ ___ ___ \n");
+
+    for(j = 0; j < 3; j++){
+      printf("| %c ", tab[i][j]);
+    }
+  }
+
+  printf("|\n ___ ___ ___ \n");
+
+}
+
+
+bool verify(char t[3][3], char p){
+  bool win = false;
+  int i, j, temp = 0;
+
+  for(i = 0; i < 3; i++){       //  Verifica condicao de vitoria horizontal
+    for(j = 0; j < 3; j++){
+      if((t[i][j]) == p){
+        temp++;
+      }
+    }
+    if(temp == 3){
+      win = true;
+    }
+    temp = 0;
+  }
+
+  for(j = 0; j < 3; j++){	//  Verifica condicao de vitoria vertical
+    for(i = 0; i < 3; i++){
+      if((t[i][j]) == p){
+	temp++;
+      }
+    }
+    if(temp == 3){
+      win = true;
+    }
+    temp = 0;
+  }
+
+  j = 0;
+
+  for(i = 0; i < 3; i++){	//  Verifica condicao de vitoria diagonal (top esq - bot dir)
+    if(t[i][j++] == p){
+      temp++;
+    }
+  }
+
+  if(temp == 3){
+    win = true;
+  }
+  temp = 0;
+
+  j = 3;
+
+  for(i = 0; i < 3; i++){	//  Verifica condicao de vitoria diagonal (top dir - bot esq)
+    if(t[i][--j] == p){
+      temp++;
+    }
+  }
+
+  if(temp == 3){
+    win = true;
+  }
+  temp = 0;
+
+  return win;
+}
+
+void jogadaOponente(char t[3][3], char p){
+  int i, j;
+  bool checking = true;
+
+  for(i = 0; i < 3; i++){
+    for(j = 0; j < 3; j++){
+      if(t[i][j] == ' ' && checking){
+	t[i][j] = p;
+	checking = false;
+      }
+    }
+  }
+}
---
+++ funcoes.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void showTab(char tab[3][3]);
+
+bool verify(char t[3][3], char p);
+
+void jogadaOponente(char t[3][3], char p);
